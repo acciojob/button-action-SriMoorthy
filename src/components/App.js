b9ef9fb655113df77f1bb15154d873b803c36b5d
SriMoorthy <srimoorthy27@gmail.com>
2024-10-12 11:24:07 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = (props) => {
   return (
     <div className="App" id="main">
       <button id='click' onClick={HandleClick}>Click me</button>
-      <p id="para" className={isVisible ? 'hide' : 'show'}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+      <p id="para" className={isVisible ? 'show' : 'hide'}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
     </div>
   );
 }
